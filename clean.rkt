576b15abf9cbf34f445e5d1c219b8b8f7195b56e
Arturo Olivares <113034629+aolgzz@users.noreply.github.com>
2022-09-16 15:49:06 -0500

    Main script v. 2.0

--- clean.rkt
+++ clean.rkt
@@ -10,62 +10,67 @@
 #lang racket
 
 (define arch (open-input-file "backend.txt"))
+(define arch_2 (open-input-file "baseDatos.txt"))
 (define transactions (read arch))
+(define backend (read arch_2))
 
 (close-input-port arch)
+(close-input-port arch_2)
 
 (define matriz (map cadr transactions))
 
-;(define n-list (list)) Lista vacía
+;(define n-list (list)) ;Lista vacía
 
 (define a (open-output-file "recibo.txt" #:exists 'replace))
 
 (define (suma-filas matrix)
   (cond
-    [(empty? matrix) (printf"No hay más transacciones")]
+    [(empty? matrix) (printf "--No hay más transacciones--\n")]
     [else
      (cond
        [(empty? (car matrix)) (writeln "No se insertaron monedas" a)]
-       ;Imprime con formato la suma de cada uno de los elementos de la matriz:
-       ;[else (printf "Sum: ~a\n" (apply +(car matrix)))(suma-filas(cdr matrix))])]))
-       ;Imprime las sumas de las filas todas pegadas en manera de lista
-       ;[else (print (list(apply +(car matrix))))(suma-filas(cdr matrix))])]))
-       ;Imprime las sumas de las filas todas pegadas
-       ;[else (print (apply +(car matrix)))(suma-filas(cdr matrix))])]))
-       ;Escribe en cada linea de un archivo la suma de cada de una de las filas
-       ;[else (writeln (apply +(car matrix)) a)(suma-filas(cdr matrix))])]))
-       ;Escribe las sumas de las filas separadas por espacios en blancos
        [else (write (apply +(car matrix)) a)(write-char #\space a)(suma-filas(cdr matrix))])]))
+
 
 (suma-filas matriz)
 
-;(transactions)
+(define (convert str)
+    (cond
+    [(symbol? str)(symbol->string str)]
+    [else (number? str)(number->string str)]))
 
-(define (validar-tokens tokens transactions)
+(define (validar-tokens input-ids machines-ids)
   (cond
-    [(empty? transactions) (printf "--No hay más transacciones--\n\n")]
+    [(and (empty? machines-ids) (empty? input-ids))
+     '()]
+    [(empty? input-ids)
+     '()]
     [else
-     (if (member (car (car transactions)) tokens)
-         (printf "Producto seleccionado válido\n");((validar-monedas (map car caja) transacciones))
-         (printf "Producto seleccionado NO válido\n"))
-     (validar-tokens tokens (rest transactions))]))
+     (if (member (car input-ids) machines-ids)
+         (cons (string-append (convert(car input-ids)) ":Valid") (validar-tokens (cdr input-ids) machines-ids))
+         (cons (string-append (convert(car input-ids)) ":Invalid") (validar-tokens (cdr input-ids) machines-ids)))]))
 
-;(validar-tokens (map car transactions) transactions)
-
-
-#|
-     (if (member (car (car transactions)) tokens)
-         (compra);((validar-monedas (map car caja) transacciones))
-         (declined))
-     (validar-tokens tokens (rest transactions))]))
-
-|#
+;(define in-ids(map car transactions))
+;(define mac-ids(map car backend))
+;(validar-tokens (map car transactions) (map car backend))
+(define primera-parte(validar-tokens (map car transactions) (map car backend)))
 
 
+(define (converter lst)
+  (if (null? lst)
+      '()
+      (cons (list (car lst)) (converter (cdr lst)))))
 
+(define (imprimir-recibo b e ls)
+    (if (<= b e)
+         (begin
+             (fprintf a "Transacción #~a\n~a\n" b (caar ls))
+             (imprimir-recibo (+ b 1) e (cdr ls)))(display "END")))
 
-
+;(imprimir-recibo 1 (length transactions) (converter primera-parte))
 
 (close-output-port a)
+
+
 
 
