c4b8b8e3ca06eb0c523c286d5bc8ddd79f8fd52a
Arturo Olivares <113034629+aolgzz@users.noreply.github.com>
2022-09-17 07:43:49 -0500

    Update vending machine (v. 3.0)

--- clean.rkt
+++ clean.rkt
@@ -19,20 +19,30 @@
 
 (define matriz (map cadr transactions))
 
-;(define n-list (list)) ;Lista vacía
-
 (define a (open-output-file "recibo.txt" #:exists 'replace))
 
 (define (suma-filas matrix)
   (cond
-    [(empty? matrix) (printf "--No hay más transacciones--\n")]
+    [(empty? matrix) empty]
     [else
      (cond
-       [(empty? (car matrix)) (writeln "No se insertaron monedas" a)]
-       [else (write (apply +(car matrix)) a)(write-char #\space a)(suma-filas(cdr matrix))])]))
+       [(empty? (car matrix)) empty]
+       [else (cons (apply +(car matrix)) (suma-filas(cdr matrix)))])]))
 
+(define (truth-or-false matrix)
+  (cond
+    [(empty? matrix) empty]
+    [else
+      (cond
+        [(empty? (car matrix)) empty]
+        [else(cons (regexp-match #rx"Valid" (caar matrix))(truth-or-false(cdr matrix)))])]))
+        ;[else (regexp-match #rx"Invalid" (caar matrix))])]))
+        #|
+        [else (cond
+                [(equal? (regexp-match #rx"Invalid" (caar matrix)) #f)(cons "False"(truth-or-false(cdr matrix)))]
+                [else (cons "True" (truth-or-false(cdr matrix)))])])]))|#
 
-(suma-filas matriz)
+;(suma-filas matriz)
 
 (define (convert str)
     (cond
@@ -47,13 +57,40 @@
      '()]
     [else
      (if (member (car input-ids) machines-ids)
-         (cons (string-append (convert(car input-ids)) ":Valid") (validar-tokens (cdr input-ids) machines-ids))
-         (cons (string-append (convert(car input-ids)) ":Invalid") (validar-tokens (cdr input-ids) machines-ids)))]))
+         (cons (string-append (convert(car input-ids)) " (Valid ID)") (validar-tokens (cdr input-ids) machines-ids))
+         (cons (string-append (convert(car input-ids)) " (Invalid ID)") (validar-tokens (cdr input-ids) machines-ids)))]))
 
 ;(define in-ids(map car transactions))
 ;(define mac-ids(map car backend))
 ;(validar-tokens (map car transactions) (map car backend))
 (define primera-parte(validar-tokens (map car transactions) (map car backend)))
+
+(define inval-t-id (remove* (map car backend) (map car transactions)))
+(define val-t-id(remove* inval-t-id (map car transactions)))
+
+
+(define (hash-table ls ls2)
+  (cond
+    [(and (empty? ls2) (empty? ls))
+     '()]
+    [(empty? ls)
+     '()]
+    [else
+        (cond
+          [(member (car ls) (car ls2))
+           ;(printf "Access granted")
+           (member (car ls) (car ls2))]
+          [else
+           (hash-table ls (cdr ls2))])]))
+
+(define (hash-table-aux ls ls2)
+  ;((data-base ls backend))
+  (if (empty? ls)
+      empty
+      (append (list (hash-table ls ls2))
+              (hash-table-aux (cdr ls) ls2))))
+
+;(hash-table-aux val-t-id backend)
 
 
 (define (converter lst)
@@ -61,16 +98,43 @@
       '()
       (cons (list (car lst)) (converter (cdr lst)))))
 
-(define (imprimir-recibo b e ls)
-    (if (<= b e)
-         (begin
-             (fprintf a "Transacción #~a\n~a\n" b (caar ls))
-             (imprimir-recibo (+ b 1) e (cdr ls)))(display "END")))
+(define (imprimir-recibo b e ls ls2 l3 l4)
+  (if (<= b e)
+      (begin
+        (fprintf a "Transacción #~a\n
+Item ID: ~a\n" b (caar ls))
+        (if (equal? (regexp-match #rx"Valid"(caar ls)) '("Valid"))
+            (fprintf a "Producto: ~a
+Total a pagar: $~a.00
+Cantidad recibida: $~a.00
+Estado: Transacción realizada\n\n"(car l3)(car l4)(car ls2))
+            (fprintf a "Producto: Unknown
+Estado: Transacción no Realizada\n\n"))
+        (imprimir-recibo (+ b 1) e (cdr ls)(cdr ls2) (cdr l3)(cdr l4)))
+      (display "END")))
 
-;(imprimir-recibo 1 (length transactions) (converter primera-parte))
+(define madworld(map caddr (hash-table-aux val-t-id backend)))
+(define analog(map cadr (hash-table-aux val-t-id backend)))
+
+(define (duplicate-until-reached lst num)
+  (cond
+    ((= num 0) '())
+    (else
+     (append lst (duplicate-until-reached lst (- num 1))))))
+
+;(duplicate-until-reached madworld (length transactions))
+
+(imprimir-recibo 
+1 
+(length transactions) 
+(converter primera-parte) 
+(suma-filas matriz)
+(duplicate-until-reached madworld (length transactions))
+(duplicate-until-reached analog (length transactions)))
+
+
+;(map caddr (hash-table-aux val-t-id backend))
+(define prima(map car backend))
+(define primo(map car transactions))
 
 (close-output-port a)
-
-
-
-
